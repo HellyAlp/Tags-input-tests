f4cf6a154bbab0f41d0af4df16d7244988218c54
Helly_Alpern <123380510+rachelalpern@users.noreply.github.com>
2023-03-06 19:26:28 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
   viewportHeight: 1080,
   e2e: {
     baseUrl: 'https://qaplayground.dev/apps/tags-input-box/',
-    supportFile: false
+    supportFile: false,
+    specPattern: '/home/runner/work/Tags-input-tests/cypress/e2e/*.cy.{js,jsx,ts,tsx}'
   },
 })
