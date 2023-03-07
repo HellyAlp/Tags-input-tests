5147e748cd18e4cf49d42777dd74b19c389c22d1
Helly_Alpern <123380510+rachelalpern@users.noreply.github.com>
2023-03-07 16:32:37 +0200

    Update inputTagsTest.cy.js

--- cypress/e2e/inputTagsTest.cy.js
+++ cypress/e2e/inputTagsTest.cy.js
@@ -1,116 +1,136 @@
 /// <reference types="cypress" />
+import _ from "lodash";
+
 describe('Test suite', () =>{
+    const defaultTags = ['node', 'javascript']
+    const maxTags = 10
+    const enter = '{enter}'
+    const tags = ['Java', 'Git', 'React', 'Linux', 'Python', 'C++','C', 'Bitbuckt', 'ios', 'Android', 'Go', 'Perl', 'Redux']
+    const singleChar = '$'
+    var tagListCount = 2
+
+    beforeEach(() => {
+        cy.visit('/')
+        tagListCount = 2
+    })
 
     describe('Deafult tags tests', () =>{
         it('Verify that the tags box contains the default tags "node" and "javascript"', () =>{
-            cy.visit('/')
-            getTagsBox().should('contain','node')
-            getTagsBox().should('contain','javascript')
-
+            defaultTags.forEach((tag) => {
+                getTagsBox().should('contain', tag)
+            })
         })
     
-        it('Verify that with deafult tags, onlt 8 tags remaining', () =>{
-            cy.visit('/')
-            getDetails().should('contain','8')
+        it('Verify that with deafult tags, only 8 tags remaining', () =>{
+            const remainingTags = maxTags - defaultTags.length
+            getDetails().should('contain',remainingTags)
         })
     
     })
 
     describe('Tags Insertion tests', () =>{
         it('Verify that the user can insert a tag by pressing the enter key', () =>{
-            cy.visit('/')
-            getTagsBox().find('input').type('Python{enter}')
-            getTagsBox().should('contain','Python')
+            const tagToAdd = _.sample(tags)
+            getTagsBox().find('input').type(`${tagToAdd}${enter}`)
+            getTagsBox().should('contain',tagToAdd)
         })
 
         it('Verify that the user can insert multiple tags by adding a comma after each tag name', () =>{
-            cy.visit('/')
-            getTagsBox().find('input').type('Java, Git, React, Linux{enter}')
-            getTagsBox().should('contain','Java').and('contain','Git').and('contain','React').and('contain','Linux')
+            const tagsToAdd = _.sampleSize(tags,maxTags/2)
+            getTagsBox().find('input').type(tagsToAdd.join()+enter)
+            for(let i=0; i < tagsToAdd.length; i++)
+            {
+                getTagsBox().should('contain',tagsToAdd[i])
+            }
         })
 
         it('Verify that the tags length should be more than one character - Single insert', () =>{
-            cy.visit('/')
-            getTagsBox().find('input').type('J{enter}')
-            getTagsBox().should('not.contain','J')
+            getTagsBox().find('input').type(singleChar+enter)
+            getTagsBox().should('not.contain',singleChar)
         })
 
         it('Verify that the tags length should be more than one character - Multi insert', () =>{
-            cy.visit('/')
-            getTagsBox().find('input').type('Java, Git, React, Linux,J{enter}')
-            getTagsBox().should('not.contain','J')
+            const tagsToAdd = _.sampleSize(tags,maxTags/2) + singleChar
+            getTagsBox().find('input').type(tagsToAdd.join()+enter)
+            getTagsBox().should('not.contain',singleChar)
         })
 
         it('Verify that the tags should not be duplicated - Single insert', () =>{
-            cy.visit('/')
-            getTagsBox().find('input').type('node{enter}')
-            getTagsBox().get('li:contains(node)').should('have.length', 1)
+            const dup = _.sample(defaultTags)
+            getTagsBox().find('input').type(dup+enter)
+            getTagsBox().get(`li:contains(${dup})`).should('have.length', 1)
         })
 
         it('Verify that the tags should not be duplicated - Multi insert', () =>{
-            cy.visit('/')
-            getTagsBox().find('input').type('Java, node, node, node,javascript{enter}')
-            getTagsBox().get('li:contains(node)').should('have.length', 1)
+            var tagsToAdd = _.sampleSize(tags,maxTags/2)
+            const dup = _.sample(tagsToAdd)
+            tagsToAdd.push(dup)
+            getTagsBox().find('input').type(tagsToAdd.join()+enter)
+            getTagsBox().get(`li:contains(${dup})`).should('have.length', 1)
         })
 
         it('Verify that the maximum number of tags in the tags box is 10 - single insert', () =>{
-            cy.visit('/')
-            var genArr = Array.from({length:10},(v,k)=>k+1)
+            var genArr = Array.from({length:maxTags},(v,k)=>k+1)
             cy.wrap(genArr).each((index) => {
-                cy.get('.content').find('input').type('Java'+index+'{enter}')
+                getTagsBox().find('input').type(_.sample(tags)+index+enter)
             })
             getDetails().should('contain','0')
-            getTagsBox().get('li').should('have.length', 10)
+            getTagsBox().get('li').should('have.length', maxTags)
 
         })
 
         it('Verify that the maximum number of tags in the tags box is 10 - Multi insert', () =>{
-            cy.visit('/')
-            cy.get('.content').find('input').type('Java1,Java2,Java3,Java4,Java5,Java6,Java7,Java8,Java9,Java10{enter}')
+            const tagsToAdd = _.sampleSize(tags,maxTags)
+            getTagsBox().find('input').type(tagsToAdd.join()+enter)
             getDetails().should('contain','0')
-            cy.get('.content').get('li').should('have.length', 10)
+            getTagsBox().get('li').should('have.length', maxTags)
         })
 
         it('Verify that the remaining tags indication updates after inserting a new tag', () =>{
-            cy.visit('/')
-            cy.get('.content').find('input').type('React{enter}')
-            getDetails().should('contain','7')
-            cy.get('.content').find('input').type('Java, Git, Mongo, Linux{enter}')
-            getDetails().should('contain','3')
+            getTagsBox().find('input').type(_.sample(tags)+enter)
+            tagListCount++
+            getDetails().should('contain',maxTags-tagListCount)
+            const tagsToAdd = _.sampleSize(tags,maxTags/2)
+            tagListCount = tagListCount + tagsToAdd.length
+            getTagsBox().find('input').type(tagsToAdd.join()+enter)
+            getDetails().should('contain',maxTags-tagListCount)
         })
     })
 
     describe('Tags Deletion tests', () =>{
         it('Verify that the user can delete a deafult tag by clicking on the remove icon', () =>{
-            cy.visit('/')
-            cy.get('.content').get('li:contains(node)').find('.uit').click()
-            cy.get('.content').should('not.contain','node')
-            getDetails().should('contain','9')
+            const tag = _.sample(defaultTags)
+
+            getTagsBox().get(`li:contains(${tag})`).find('.uit').click()
+            tagListCount--
+            getTagsBox().should('not.contain',tag)
+            getDetails().should('contain',maxTags-tagListCount)
         })
 
         it('Verify that the user can delete a tag after insertation', () =>{
-            cy.visit('/')
-            getTagsBox().find('input').type('Python{enter}')
-            getDetails().should('contain','7')
-            getTagsBox().get('li:contains(Python)').find('.uit').click()
-            getTagsBox().should('not.contain','Python')
-            getDetails().should('contain','8')
+            const tag = _.sample(tags)
+            getTagsBox().find('input').type(tag+enter)
+            tagListCount++
+            getDetails().should('contain',maxTags-tagListCount)
+            getTagsBox().get(`li:contains(${tag})`).find('.uit').click()
+            tagListCount--
+            getTagsBox().should('not.contain',tag)
+            getDetails().should('contain',maxTags-tagListCount)
         })
 
         it('Verify that the "Remove All" button removes all the tags from the tags box - Deafult', () =>{
-            cy.visit('/')
             cy.get('.details button').click()
             getTagsBox().get('li').should('have.length', 0)
-            getDetails().should('contain','10')
+            getDetails().should('contain',maxTags)
         })
 
         it('Verify that the "Remove All" button removes all the tags from the tags box - After insertation', () =>{
-            cy.visit('/')
-            getTagsBox().find('input').type('Java1,Java2,Java3,Java4,Java5,Java6,Java7, Java8{enter}')
+            const tagsToAdd = _.sampleSize(tags,maxTags-defaultTags.length)
+            getTagsBox().find('input').type(tagsToAdd.join()+enter)
             getDetails().should('contain','0')
             cy.get('.details button').click()
             getTagsBox().get('li').should('have.length', 0)
-            getDetails().should('contain','10')
+            getDetails().should('contain',maxTags)
         })
     })
 })
